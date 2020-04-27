4f2730b775d33a2e7bf881ef150ae4e4dcaa4795
MiguelZN <miguelzavalan@gmail.com>
2020-04-27 00:04:40 -0400

    created board class, added method get Tiles by unique index

--- qlearning.py
+++ qlearning.py
@@ -7,6 +7,7 @@ Dr.Rahmat
 '''
 
 from enum import Enum
+import math
 
 class TILE_TYPES(Enum):
     START = 'start'
@@ -32,13 +33,79 @@ class TILE_TYPES(Enum):
         return False
 
 class Tile:
-    def __init__(self, type=TILE_TYPES.ORDINARY):
+    def __init__(self, type=TILE_TYPES.ORDINARY,unique_index:int=-1):
         self.value = None
         self.type = type
+        self.unique_index = unique_index
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "("+str(self.type)+"|Index "+str(self.unique_index)+")"
 
 class Board(list):
-    def __init__(self):
-        ''
+    def __init__(self,n_Rows:int=4,n_Columns:int=4):
+        self.n_Rows = n_Rows
+        self.n_Columns = n_Columns
+        self.size = self.n_Rows*self.n_Columns
+
+        self.createBoard(self.n_Rows,self.n_Columns)
+
+    def createBoard(self,n_Rows:int,n_Columns:int)->None:
+        self.clear() #Clears the current board
+
+        curr_index = 1
+        for r in range(n_Rows):
+            current_row = []
+            for c in range(n_Columns):
+                current_row.append(Tile(unique_index=curr_index))
+                curr_index+=1
+
+            #Adding the row to the board
+            self.insert(0,current_row)
+
+    def getTileUniqueIndex(boardObject,unique_index:int)->Tile:
+        row = (boardObject.n_Rows)-math.floor(unique_index/boardObject.n_Rows)-1
+        column = (unique_index-1)%(boardObject.n_Columns)
+
+        #Readjusts the row if the row is divisible by the number of rows in the board
+        if(unique_index!=0 and unique_index%boardObject.n_Rows==0):
+            row +=1
+
+        if(row<0 or row>=boardObject.n_Rows):
+            return None
+        if(column<0 or column>=boardObject.n_Columns):
+            return None
+
+        #print(row,column)
+
+        return boardObject[row][column]
+
+    def printBoard(boardObject):
+        print("BOARD:")
+        for i in range(boardObject.n_Rows):
+            currRow = ""
+            for j in range(boardObject.n_Columns):
+                currRow+=str(boardObject[i][j])
+
+            print(currRow)
+
+
+
+
+
+
+
+
 
 print(TILE_TYPES.ORDINARY == 'ordinary')
 print(TILE_TYPES.ORDINARY == TILE_TYPES.ORDINARY)
+
+print(Board())
+Board1 = Board()
+print(Board.getTileUniqueIndex(Board1,16))
+Board.getTileUniqueIndex(Board1,16).type=TILE_TYPES.START
+print(Board.getTileUniqueIndex(Board1,16))
+
+Board.printBoard(Board1)
